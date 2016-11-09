54fe6a55caf2eafee4a1febfa5e34e126c1dcc84
sawangpong muadphet <admin@localhost.localdomain>
2016-11-09 23:08:59 +0700

    Commit

---
+++ set_chrony.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+conf="/etc/chrony.conf"
+sed -i.back '3,6d' $conf
+sed -i '19i allow 10.10.10.0/24' $conf
+sed -i '3i server 1.th.pool.ntp.org iburst' $conf
+sed -i '4i server 0.asia.pool.ntp.org iburst' $conf
+sed -i '5i server 2.asia.pool.ntp.org iburst' $conf 
+
+sudo systemctl restart chronyd
+sleep 3
+chronyc sources
+
+hosts="mds1 mds2 mon1 mon2 mon3 osd1 osd2 osd3 osd4"
+for i in $hosts;do
+scp chrony-client.sh  root@${i}:/root 
+ssh $i /root/chrony-client.sh
+done 
